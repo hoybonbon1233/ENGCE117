0f9e689979e96eb82ae9991d63aa682f169a9141
hoybonbon1233 <Kinghoop12345@gmail.com>
2025-12-11 12:13:31 +0700

    zzz

---
+++ lab11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n ;
+    int i ;
+
+   
+    scanf("%d", &n) ;
+
+   
+    for ( i = 1 ; i <= n ; i++ ) 
+    {
+        printf("[%d] Hello world\n", i) ;
+    }
+
+    return 0 ;
+}
