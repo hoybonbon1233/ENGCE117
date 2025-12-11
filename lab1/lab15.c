5196c4003667a027d27254cbb92e662deb29330c
hoybonbon1233 <Kinghoop12345@gmail.com>
2025-12-11 13:48:54 +0700

    zzz

--- lab1/lab15.c
+++ lab1/lab15.c
@@ -1,44 +1,34 @@
 #include <stdio.h>
 
-int matrix[1000] ;
 
-
-void GetMatrix(int **value, int *row, int *col) {
-    
-    int i, j ;
-
-    
-    printf("How many row, col ? \n") ;
-    scanf("%d %d", row, col) ;
-
-    *value = matrix ;
-
-   
-    for (i = 0 ; i < *row ; i++) {
-        for (j = 0; j < *col; j++) {
-            
-            printf("a[%d][%d] = ", i, j)  ;
-             
-          
-            scanf("%d", &matrix[i * (*col) + j]) ;
-        }
-    }
-}
 
 int main() {
-    int *data , m , n ;
-    int i , j ; 
-
-    GetMatrix(&data, &m, &n) ;
+    int number ;
+    int evenSum = 0 ; 
+    int oddSum = 0 ;   
 
     
-    printf("\nMatrix:\n") ;
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d ", data[i * n + j]) ;
-        }
-        printf("\n") ;
+    if (scanf("%d", &number) != 1) {
+        return 1 ;
     }
 
+   
+    while (number != 0) {
+        
+        if (scanf("%d", &number) != 1) {
+            break;
+        }
+
+        
+        if (number % 2 == 0) {
+            evenSum += number ;  
+        } else {
+            oddSum += number ;   
+        }
+    }
+
+   
+    printf("Even Sum: %d\n", evenSum) ;
+    printf("Odd Sum: %d\n", oddSum) ;
     return 0 ;
 }
