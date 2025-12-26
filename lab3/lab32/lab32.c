a2b51034f660fac387f50440edae2bfa0710d954
hoybonbon1233 <Kinghoop12345@gmail.com>
2025-12-26 12:57:43 +0700

    zzz

---
+++ lab3/lab32/lab32.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct student {
+	char name[20] ;
+	int age ;
+	char sex ;
+	float gpa ;
+} ;
+
+void upgrade(struct student *Child) {
+	if (Child->sex == 'M') {
+		Child->gpa = Child->gpa * 1.10f ;
+	} else if (Child->sex == 'F') {
+		Child->gpa = Child->gpa * 1.20f ;
+		if (Child->gpa > 4.0f) Child->gpa = 4.0f ;
+	}
+}
+
+int main(void) {
+	struct student Aboy ;
+	Aboy.sex = 'M' ;
+	Aboy.gpa = 3.00f ;
+	upgrade(&Aboy) ;
+	printf( "%.2f\n", Aboy.gpa ) ;
+	return 0 ;
+}
+
